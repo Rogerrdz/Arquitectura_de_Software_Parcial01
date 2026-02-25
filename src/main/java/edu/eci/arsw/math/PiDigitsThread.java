39177271f7debcc63f0247469be0e29d5683a986
Roger rdz <142515714+Rogerrdz@users.noreply.github.com>
2026-02-25 18:41:23 -0500

    Agregando PiDigitsThread para el 3 punto

--- src/main/java/edu/eci/arsw/math/PiDigitsThread.java
+++ src/main/java/edu/eci/arsw/math/PiDigitsThread.java
@@ -1,5 +1,8 @@
 package edu.eci.arsw.math;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class PiDigitsThread extends Thread {
 
     private int start;
@@ -13,8 +16,28 @@ public class PiDigitsThread extends Thread {
 
     @Override
     public void run() {
-        result = PiDigits.getDigits(start, count);
+        long last = System.currentTimeMillis();
+        int processed = 0;
+        result = new byte[count];
+
+        for (int i = 0; i < count; i++) {
+            result[i] = PiDigits.getDigits(start + i, 1)[0];
+            processed++;
+
+            if (System.currentTimeMillis() - last >= 5000) {
+                System.out.println(
+                    "Hilo " + this.getId() + " procesó " + processed + " dígitos. Presione ENTER para continuar."
+                );
+                try {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                    br.readLine();
+                } catch (Exception e) {
+                }
+                last = System.currentTimeMillis();
+            }
+        }
     }
+    
 
     public byte[] getResult() {
         return result;
